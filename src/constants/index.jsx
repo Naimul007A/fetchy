34238d56a6dcd0239e993879428c93984cc49fb1
PRAS <prassamin@gmail.com>
2025-01-21 03:09:25 +0600

    v2.0.3i - Test Redirection url fetching work on production or not

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,5 +1,5 @@
 export const _cdnHost = 'https://cdn.prassamin.workers.dev';
-export const _tiktokVideoFetchApiByPRAS = `${_cdnHost}/api/tiktok/video`;
+export const _tiktokVideoFetchApiByPRAS = `${_cdnHost}/api/tiktok/video?url=`;
 export const _tiktokProfileFetchApiByPRAS = `${_cdnHost}/api/tiktok/profile?url=`;
 export const _PRASCdnMergeItApi = `${_cdnHost}/api/mergeit`;
 export const _PRASCdnServeitApi = `${_cdnHost}/api/serveit`;
