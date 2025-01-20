153716e78aa38556aebf6196561831fd28dc27c9
PRAS <prassamin@gmail.com>
2025-01-20 19:48:30 +0600

    v2.0.3a - Test Redirection url fetching work on production or not

--- src/lib/facebook/index.jsx
+++ src/lib/facebook/index.jsx
@@ -12,8 +12,12 @@ const isRedirectorUrl = (url) => {
 export const resolveRedirectUrl = async (url) => {
     try {
         const response = await axios.get(url, {
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            },
             maxRedirects: 0,
-            validateStatus: (status) => status >= 200 && status < 400
+            validateStatus: (status) => status >= 200 && status < 400,
         });
 
         if (response.headers.location) {
