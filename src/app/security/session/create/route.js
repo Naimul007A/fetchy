8ce02af7215640f7c0ac5ba02b0170991cdf49ee
PRAS <prassamin@gmail.com>
2025-05-30 03:07:29 +0600

    test: production env issue t3

--- src/app/security/session/create/route.js
+++ src/app/security/session/create/route.js
@@ -13,6 +13,7 @@ export async function POST(req) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
+  console.log(ipAddress(req), req.headers.get("x-forwarded-for"));
   const token = manager.createToken({
     ip: ipAddress(req) || req.headers.get("x-forwarded-for"),
     userAgent: req.headers.get("x-user-agent"),
