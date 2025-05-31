843850d7cab71335a7a1de6e975458037eb545e6
PRAS <prassamin@gmail.com>
2025-05-31 22:44:01 +0600

    fix: broken ui on small vh devices

--- src/app/home/components/hero.jsx
+++ src/app/home/components/hero.jsx
@@ -7,23 +7,23 @@ import Galaxy from "./galaxy";
 
 const HeroSection = () => {
     return (
-        <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10 mx-auto">
-            <div className="text-center lg:text-start space-y-6 h-[calc(100vh-10rem-57px)] md:h-[calc(100vh-16rem-57px)] flex flex-col justify-between md:justify-evenly">
+        <section className="w-[calc(100vw-2rem)] sm:container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10 mx-auto">
+            <div className="min-h-[calc(100vh-10rem-57px)] md:min-h-[calc(100vh-16rem-57px)] flex flex-col justify-evenly">
                 <div className="flex flex-col space-y-6">
-                    <main className="text-3xl modern:text-5xl md:text-6xl font-bold">
-                        <h1 className="inline">
+                    <div className="text-3xl modern:text-5xl md:text-6xl font-bold flex">
+                        <h1 className="inline text-center lg:text-left">
                             <span className="inline bg-gradient-to-r from-[#7837d1] to-[#cba6ff] text-transparent bg-clip-text">
                                 Fetchy,
                             </span>{" "}
                             Download Anything, Anywhere, Anytime.
                         </h1>
-                    </main>
-                    <p className="text-md modern:text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
+                    </div>
+                    <p className="text-md text-center lg:text-left modern:text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
                         Fetchy isn’t just another free video downloader. It’s your all-in-one tool to save everything from Instagram, Facebook, and TikTok — no watermarks, no fluff, just speed and simplicity.
                         Whether it’s a reel, post, photo, or even a Facebook story, Fetchy’s got your back. Many other tools miss the mark — Fetchy hits it dead center.
                     </p>
                 </div>
-                <div className="space-y-4 md:space-y-0 md:space-x-4 px-2 md:px-0">
+                <div className="gap-4 md:gap-4 px-2 md:px-0 flex flex-col md:flex-row lg:justify-start justify-center">
                     <Button asChild className="w-full md:w-1/3">
                         <Link href="/tools">Get Started</Link>
                     </Button>
